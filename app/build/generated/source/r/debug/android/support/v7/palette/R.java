3cc1237e7484fbec24cbfd18f5b6333027429e1a
PequenoAprendiz <berews02@gmail.com>
2018-03-26 01:11:45 -0300

    melhoria no layout do listview

--- app/build/generated/source/r/debug/android/support/v7/palette/R.java
+++ app/build/generated/source/r/debug/android/support/v7/palette/R.java
@@ -64,31 +64,31 @@ public final class R {
         public static final int notify_panel_notification_icon_bg = 0x7f08019e;
     }
     public static final class id {
-        public static final int action_container = 0x7f090017;
-        public static final int action_divider = 0x7f090019;
-        public static final int action_image = 0x7f09001a;
-        public static final int action_text = 0x7f090021;
-        public static final int actions = 0x7f090022;
-        public static final int async = 0x7f090033;
-        public static final int blocking = 0x7f09003a;
-        public static final int chronometer = 0x7f090061;
-        public static final int forever = 0x7f090095;
-        public static final int icon = 0x7f0900a3;
-        public static final int icon_group = 0x7f0900a4;
-        public static final int info = 0x7f0900b0;
-        public static final int italic = 0x7f0900b8;
-        public static final int line1 = 0x7f0900c2;
-        public static final int line3 = 0x7f0900c3;
-        public static final int normal = 0x7f0900f6;
-        public static final int notification_background = 0x7f0900f7;
-        public static final int notification_main_column = 0x7f0900f8;
-        public static final int notification_main_column_container = 0x7f0900f9;
-        public static final int right_icon = 0x7f090115;
-        public static final int right_side = 0x7f090116;
-        public static final int text = 0x7f09014e;
-        public static final int text2 = 0x7f090150;
-        public static final int time = 0x7f09016d;
-        public static final int title = 0x7f09016e;
+        public static final int action_container = 0x7f090012;
+        public static final int action_divider = 0x7f090014;
+        public static final int action_image = 0x7f090015;
+        public static final int action_text = 0x7f09001c;
+        public static final int actions = 0x7f09001d;
+        public static final int async = 0x7f09002e;
+        public static final int blocking = 0x7f090035;
+        public static final int chronometer = 0x7f09005c;
+        public static final int forever = 0x7f090090;
+        public static final int icon = 0x7f09009e;
+        public static final int icon_group = 0x7f09009f;
+        public static final int info = 0x7f0900ab;
+        public static final int italic = 0x7f0900b3;
+        public static final int line1 = 0x7f0900c0;
+        public static final int line3 = 0x7f0900c1;
+        public static final int normal = 0x7f0900f4;
+        public static final int notification_background = 0x7f0900f5;
+        public static final int notification_main_column = 0x7f0900f6;
+        public static final int notification_main_column_container = 0x7f0900f7;
+        public static final int right_icon = 0x7f090113;
+        public static final int right_side = 0x7f090114;
+        public static final int text = 0x7f09014c;
+        public static final int text2 = 0x7f09014e;
+        public static final int time = 0x7f09016c;
+        public static final int title = 0x7f09016d;
     }
     public static final class integer {
         public static final int status_bar_notification_info_maxnum = 0x7f0a000e;
