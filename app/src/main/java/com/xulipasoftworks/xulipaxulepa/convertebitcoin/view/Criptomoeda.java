803d82842228fe24219e41a877214c297b9a32ef
xulipaxulepa <aofnas@hotmail.com>
2018-03-25 21:53:41 -0300

    Versão 2.5 do app finalizada, upada e funcionando na Play Store.

---
+++ app/src/main/java/com/xulipasoftworks/xulipaxulepa/convertebitcoin/view/Criptomoeda.java
@@ -0,0 +1,107 @@
+package com.xulipasoftworks.xulipaxulepa.convertebitcoin.view;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.xulipasoftworks.xulipaxulepa.convertebitcoin.R;
+
+import java.io.Serializable;
+
+/**
+ * Created by xulipasoftworks on 25/02/2018.
+ */
+
+public class Criptomoeda {
+
+    private String id;
+    private String nome;
+    private String price_btc;
+    private String price_usd;
+    private String price_usd_unit;
+    private String price_brl;
+    private String price_brl_unit;
+    private String variacao;
+
+
+    protected Criptomoeda() {
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public String getVariacao() {
+        return variacao;
+    }
+
+    public void setVariacao(String variacao) {
+        this.variacao = variacao;
+    }
+    public String getPrice_btc() {
+        return price_btc;
+    }
+
+    public void setPrice_btc(String price_btc) {
+        this.price_btc = price_btc;
+    }
+
+    public String getPrice_usd() {
+        return price_usd;
+    }
+
+    public void setPrice_usd(String price_usd) {
+        this.price_usd = price_usd;
+    }
+
+    public String getPrice_brl() {
+        return price_brl;
+    }
+
+    public void setPrice_brl(String price_brl) {
+        this.price_brl = price_brl;
+    }
+
+    public String getPrice_usd_unit() {
+        return price_usd_unit;
+    }
+
+    public void setPrice_usd_unit(String price_usd_unit) {
+        this.price_usd_unit = price_usd_unit;
+    }
+
+    public String getPrice_brl_unit() {
+        return price_brl_unit;
+    }
+
+    public void setPrice_brl_unit(String price_brl_unit) {
+        this.price_brl_unit = price_brl_unit;
+    }
+
+
+    public int getImagen(int position){
+        switch (position){
+            case 0:
+                return(R.mipmap.bitcoin);
+            case 1:
+                return(R.mipmap.ethereum);
+            case 2:
+                return(R.mipmap.ripple);
+            default:
+                return (R.mipmap.ic_launcher);
+        }
+    }
+}
---
+++ app/src/main/java/com/xulipasoftworks/xulipaxulepa/convertebitcoin/view/ShareActivity.java
@@ -0,0 +1,51 @@
+package com.xulipasoftworks.xulipaxulepa.convertebitcoin.view;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.xulipasoftworks.xulipaxulepa.convertebitcoin.R;
+import com.facebook.FacebookSdk;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareDialog;
+
+public class ShareActivity extends AppCompatActivity {
+
+    ShareDialog shareDialog;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        facebookSDKInitialize();
+        shareDialog = new ShareDialog(this);
+        setContentView(R.layout.activity_share);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    protected void facebookSDKInitialize(){
+        FacebookSdk.sdkInitialize(getApplicationContext());
+    }
+
+    public void shareOnFacebook(View view){
+        ShareLinkContent content = new ShareLinkContent.Builder()
+                .setContentUrl(Uri.parse("https://play.google.com/store/apps/details?id=com.xulipasoftworks.xulipaxulepa.convertebitcoin&hl=pt_BR"))
+                .build();
+        shareDialog.show(content);
+    }
+
+    public void shareOnTwitter(View view){
+        String tweetUrl = "https://twitter.com/intent/tweet?text="+getString(R.string.Tweet)+" &url="
+                + "https://play.google.com/store/apps/details?id=com.xulipasoftworks.xulipaxulepa.convertebitcoin&hl=pt_BR";
+        Uri uri = Uri.parse(tweetUrl);
+        startActivity(new Intent(Intent.ACTION_VIEW, uri));
+    }
+
+}
